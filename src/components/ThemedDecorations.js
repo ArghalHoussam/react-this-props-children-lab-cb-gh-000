aed2ecc32db813fddbb737dbda7e35b08dc260f9
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-04 09:12:24 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,13 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedDecorations extends React.Component {
+  const childrenWithTheme = React.children.map(this.props.children, child => {
+    return React.cloneElement(child, { className: { "" + this.props.theme } });
+  });
+  render() {
+    return (
+      { childrenWithTheme }
+    )
+  }
+}
